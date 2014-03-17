2f885cf8adb824d87a1c8175bb0811b23e3a5b78
Egor Eremeev <ogogo.com@gmail.com>
2014-03-17 21:43:13 +0400

    Новый Twitter и разрешаем + в email

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -3,7 +3,7 @@ $("#region").chosen({no_results_text: "Регион не нейден"});
 $(document).on('submit','form',function(event){
    	var $email = $('#email');
 	if($email.val() != '') {
-		var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+		var pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
 		if(pattern.test($email.val())){
 			$email.parent().removeClass('has-error has-feedback');
 			$email.next().addClass('hide');
